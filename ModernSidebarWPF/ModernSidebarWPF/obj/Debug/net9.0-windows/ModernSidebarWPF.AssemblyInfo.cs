156bbce704703b51c3927c01f635396b3d402d1f
skyfx <eagle724eagke724@gmail.com>
2025-05-02 21:03:26 +0800

    1

--- ModernSidebarWPF/ModernSidebarWPF/obj/Debug/net9.0-windows/ModernSidebarWPF.AssemblyInfo.cs
+++ ModernSidebarWPF/ModernSidebarWPF/obj/Debug/net9.0-windows/ModernSidebarWPF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModernSidebarWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd082903554501820522385176170ce8040f4e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModernSidebarWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModernSidebarWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
